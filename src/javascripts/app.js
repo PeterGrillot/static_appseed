2c43ea3db918f195761aaebc507f063f74a0ad78
Peter Grillot <petergrillot@gmail.com>
2016-02-16 15:30:32 -0500

    Init Commit

---
+++ src/javascripts/app.js
@@ -0,0 +1,13 @@
+// vendor scirpts
+import $ from 'vendor/jquery-2.1.4.min.js';
+//import CountUp from 'vendor/countUp.js';
+
+// modules scripts
+//import 'modules/main.nav.js';
+
+$(window).load(function() {
+	
+});
+$(window).scroll(function() {
+	
+});
