ac1002063c3ec611a37b964d09ccddf816d2cd38
Peter Grillot <petergrillot@gmail.com>
2016-02-26 16:54:08 -0500

    Added b-lazy and divided up templates, added AnimateCSS

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -4,10 +4,23 @@ import $ from 'vendor/jquery-2.1.4.min.js';
 
 // modules scripts
 //import 'modules/main.nav.js';
+$(document).ready(function(){
+	$('main').addClass("fadeIn");
+	var bLazy = new Blazy();
+})
+var popImg = function (){
+	$('img').each(function() {
+	    var imagePos = $(this).offset().top;
 
-$(window).load(function() {
-	
-});
+	    var topOfWindow = $(window).scrollTop();
+	    var heightOfWindow = $( window ).height();
+	    if (imagePos < topOfWindow + heightOfWindow ) {
+	        $(this).addClass("bounceIn");
+	    }
+
+	});
+}
+popImg();
 $(window).scroll(function() {
-	
+    popImg();
 });
